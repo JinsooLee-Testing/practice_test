295626d7e660b617f0ccb409841261bd82476e5d
iris2676 <tmf2676@gmail.com>
2020-12-29 18:27:45 +0900

    Google plugin 과 Unity 서로 연결 및 로그인코드 생성함. (아직 계정 정보 미입력)

---
+++ Platform/LoginProgram/Assets/GoogleLoginManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
+public class GoogleLoginManager : MonoBehaviour
+{
+    bool bWait = false;
+
+    void Awake()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+            .RequestIdToken()
+            .Build();
+
+        PlayGamesPlatform.InitializeInstance(config);
+        // recommended for debugging:
+        PlayGamesPlatform.DebugLogEnabled = true;
+        // Activate the Google Play Games platform
+        PlayGamesPlatform.Activate();
+    }
+
+
+    public void googleLogon()
+    {
+        Social.localUser.Authenticate(success =>
+        {
+            if (success)
+            { Debug.Log("success"); }
+            else
+            { Debug.Log("fail"); }
+        });
+    }
+
+    public void OnLogOut()
+    {
+        ((PlayGamesPlatform)Social.Active).SignOut();
+        Debug.Log("Logout");
+    }
+}
