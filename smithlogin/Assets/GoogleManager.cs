6b8e2ec3cdaffcd4f79a88571239caeaf9995817
iris2676 <tmf2676@gmail.com>
2021-01-01 18:19:20 +0900

    Google Login 성공 Error Unity AndroidJavaException: java.lang.ClassNotFoundException: com.google.android.gms.games.Games 이슈 유니티 [Minify] - [Release] 및 [Debug]를 Proguard로 설정 후 custon proguard file 체크박스 활성화로 해결

---
+++ smithlogin/Assets/GoogleManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using UnityEngine.UI;
+
+public class GoogleManager : MonoBehaviour
+{
+    public Text LogText;
+
+
+    void Start()
+    {
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+        LogIn();
+    }
+
+
+    public void LogIn()
+    {
+        Social.localUser.Authenticate((bool success) =>
+        {
+            if (success) LogText.text = Social.localUser.id + " \n " + Social.localUser.userName;
+            else LogText.text = "구글 로그인 실패";
+        });
+    }
+
+
+    public void LogOut()
+    {
+        ((PlayGamesPlatform)Social.Active).SignOut();
+        LogText.text = "구글 로그아웃";
+    }
+}
