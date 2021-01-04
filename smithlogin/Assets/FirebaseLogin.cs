ffea4cb1fc26868ab48c943523072a1f11a3ada9
iris2676 <tmf2676@gmail.com>
2021-01-04 20:29:51 +0900

    파이어베이스 < -> Unity 익명 로그인 추가

--- smithlogin/Assets/FirebaseLogin.cs
+++ smithlogin/Assets/FirebaseLogin.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Firebase;
+using Firebase.Auth;
 public class FirebaseLogin : MonoBehaviour
 {
+    Firebase.Auth.FirebaseAuth auth = null;
     // Start is called before the first frame update
     void Start()
     {
-        
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void GestLoginButton()
     {
-        
+        auth.SignInAnonymouslyAsync().ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInAnonymouslyAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInAnonymouslyAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+
+    public void LogOut()
+    {
+        auth.SignOut();
+        Debug.Log("logout");
     }
 }
